fee06c82808725d3ab230dec0f810805d8d805e3
OrioMan <sielsan1@gmail.com>
2024-07-23 18:59:27 +0300

    Add files via upload

---
+++ lesson0.py
@@ -0,0 +1,9 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd program
+print((1234//10) % 100 + (5678//10)%100)
+# 4th program
+print(13 == 13 or 13 % 100 and 13 // 10)
+print(42 == 42 or 42 % 100 and 42 // 10)
